576af68addedd1f1ef59a3800460d3f7ed11b11f
KataNemu <danilzinovev333@gmail.com>
2023-09-16 11:15:23 +0300

    move files in Lab1

---
+++ Lab1/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/Shy/Documents/OOP_Labs
+# Build directory: /Users/Shy/Documents/OOP_Labs/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(BASIC "/Users/Shy/Documents/OOP_Labs/build/tests")
+set_tests_properties(BASIC PROPERTIES  _BACKTRACE_TRIPLES "/Users/Shy/Documents/OOP_Labs/CMakeLists.txt;16;add_test;/Users/Shy/Documents/OOP_Labs/CMakeLists.txt;0;")
