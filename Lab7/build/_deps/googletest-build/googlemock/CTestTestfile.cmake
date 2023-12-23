36492d357c3c0de1aa44a3a1f06287a07a3b0bbb
Crane <113766015+CraneHere@users.noreply.github.com>
2023-12-23 18:08:25 +0300

    Delete Lab7/build directory

--- Lab7/build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Shy/Documents/Object-oriented-programming-Labs/Lab7/build/_deps/googletest-src/googlemock
-# Build directory: /Users/Shy/Documents/Object-oriented-programming-Labs/Lab7/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
